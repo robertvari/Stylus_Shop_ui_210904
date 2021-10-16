9e32f023ab49cf736b5bbd15d02fbf9403ac1d55
Robert Vari <mail.pythonsuli@gmail.com>
2021-10-16 16:08:31 +0200

    update

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import "./Content.css"
 
 function Content(props) {
     return (
-        <div>
+        <div className="content-container">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dolorum eos excepturi harum id illum nesciunt placeat tenetur vero voluptate? Accusamus, aliquid architecto aut dolore doloribus eum eveniet facilis, impedit inventore ipsa ipsum labore maiores mollitia nulla odio officiis quae quis repellat rerum veniam. Dolor facere inventore sint voluptatem! A ad aliquid animi at, aut blanditiis, commodi cupiditate dignissimos ea enim exercitationem explicabo in, incidunt ipsam ipsum laboriosam maxime nesciunt nihil non nulla obcaecati odio officiis optio provident quae quasi quos reiciendis repudiandae saepe similique tempora ut velit voluptatum! Atque reprehenderit, suscipit. A asperiores ex fugit harum hic quas voluptatibus.</p>
         </div>
     );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import "./Footer.css"
 
 function Footer({title, address, phone, email}) {
     return (
-        <div>
+        <div className="footer-container">
             <div>{title}</div>
             <div>{address}</div>
             <div>{phone}</div>
