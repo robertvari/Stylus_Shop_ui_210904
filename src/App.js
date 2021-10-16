deed7bcdb88e0ae95e413b525e8897e67afbd3be
Robert Vari <mail.pythonsuli@gmail.com>
2021-10-16 15:51:26 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,58 +1,16 @@
-function Menu(){
-    return(
-        <div>
-            <a href="">Home</a>
-            <a href="">About</a>
-            <a href="">Contact</a>
-        </div>
-    )
-}
-
-function Header(){
-    return (
-        <div>
-            Header..
-            <Menu/>
-        </div>
-    )
-}
-
-function Content() {
-    return (
-        <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam asperiores at cum ducimus eaque eos exercitationem illum impedit in laudantium mollitia natus nisi numquam quidem, quos repellendus suscipit tempora, unde voluptatum. A deleniti enim, fuga minus repellat sunt. Ab aut deleniti non quas tenetur? Fugit magnam molestiae odit omnis placeat possimus quos saepe veritatis. Ab, ad aspernatur assumenda aut blanditiis dolor ducimus exercitationem facilis iure libero natus nemo odit praesentium quidem ratione recusandae rerum sunt temporibus? Adipisci dolorem error expedita explicabo facere fuga fugiat in ipsam, iusto magnam minima, molestiae neque obcaecati perferendis quam quis reiciendis repellat. Nulla, sequi!</p>
-            <hr/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis culpa cum deleniti dolorem eligendi facere illo ipsum laudantium, molestiae nobis pariatur quam quasi repellat repellendus sequi soluta tenetur. Accusantium maxime modi officiis porro quasi. Accusantium aliquam animi aut ea eius eos, facilis impedit minus nemo non ratione, repellendus reprehenderit tenetur.</p>
-        </div>
-        )
-}
-
-function Footer({title, phone, email, address}){
-    return (
-        <div>
-            <hr/>
-            <div>{title}</div>
-            <div>{phone}</div>
-            <div>{email}</div>
-            <div>{address}</div>
-        </div>
-    )
-}
+import Menu from "./components/Menu";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 function App() {
   return (
 
     <div>
-        <Header name="Robert Vari"/>
+        <Menu/>
 
         <Content/>
 
-        <Footer
-            title="STYLUS PHOTOGRAPHY"
-            phone="+36-70-3696077"
-            email="szmatefy@gmail.com"
-            address="Budapest, Hungary"
-        />
+        <Footer/>
     </div>
 
   );
---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Content(props) {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dolorum eos excepturi harum id illum nesciunt placeat tenetur vero voluptate? Accusamus, aliquid architecto aut dolore doloribus eum eveniet facilis, impedit inventore ipsa ipsum labore maiores mollitia nulla odio officiis quae quis repellat rerum veniam. Dolor facere inventore sint voluptatem! A ad aliquid animi at, aut blanditiis, commodi cupiditate dignissimos ea enim exercitationem explicabo in, incidunt ipsam ipsum laboriosam maxime nesciunt nihil non nulla obcaecati odio officiis optio provident quae quasi quos reiciendis repudiandae saepe similique tempora ut velit voluptatum! Atque reprehenderit, suscipit. A asperiores ex fugit harum hic quas voluptatibus.</p>
+        </div>
+    );
+}
+
+export default Content;
