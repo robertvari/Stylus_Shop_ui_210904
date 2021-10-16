1c3a20383474ce51058792a3fcf7df091bf2219d
Robert Vari <mail.pythonsuli@gmail.com>
2021-10-16 17:18:46 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import Menu from "./components/Menu";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Counter from "./components/Counter";
-
 function App() {
   return (
-
     <div>
-        <Header/>
-
-        <Menu/>
-
-        <Counter name="Counter 1"/>
-        <Counter name="Counter 2"/>
-        <Counter name="Counter 3"/>
-
-        <Content/>
-
-        <Footer
-            title="Stylus Photography"
-            address="Budapest, Hungary"
-            phone="+36-70-3696077"
-            email="szmatefy@gmail.com"
-        />
+        Stylus Shop
     </div>
 
   );
