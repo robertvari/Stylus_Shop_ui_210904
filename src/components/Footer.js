1cf1c9ba656621ed4001cdd335fa4ec0756a9f2b
Robert Vari <mail.pythonsuli@gmail.com>
2021-10-17 15:49:34 +0200

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,31 @@ import React from 'react';
 function Footer(props) {
     return (
         <div className="footer-container">
-            Footer
-            <input type="email" placeholder="Email"/>
-            <button>SIGN UP</button>
+
+            <div className="content-container">
+                <div>
+                    <h3>QUICK LINKS</h3>
+                    <p>Search</p>
+                    <p>Contact</p>
+                    <p>About Us</p>
+                    <p>News</p>
+                    <p>FAQ</p>
+                </div>
+
+                <div>
+                    <h3>GET IN TOUCH</h3>
+                    <p>Sign up to stay in the loop. Receive updates, access to exclusive deals, and more.</p>
+                </div>
+
+                <div>
+                    <h3>NEWSLETTER</h3>
+
+                    <div className="newsletter-box">
+                        <input type="email" placeholder="Email"/>
+                        <button>SIGN UP</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
