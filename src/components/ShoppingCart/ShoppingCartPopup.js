023baec8feaeff071fc487ef3858bccf381ce94b
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-13 15:40:17 +0100

    update

--- src/components/ShoppingCart/ShoppingCartPopup.js
+++ src/components/ShoppingCart/ShoppingCartPopup.js
@@ -1,11 +1,12 @@
-import React, {useContext} from 'react';
+import React, {useContext, Fragment} from 'react';
 import {ShoppingCartContext} from "../Contexts/ShoppingCartContext";
 import CartItem from "./CartItem";
 import {numberWithCommas} from "../../utilities";
+import {Link} from "react-router-dom";
 
 
 function ShoppingCartPopup(props) {
-    const {shopping_list, total, visible, set_visible} = useContext(ShoppingCartContext)
+    const {shopping_list, total, count, visible, set_visible} = useContext(ShoppingCartContext)
 
     if(!visible) return null
 
@@ -13,17 +14,30 @@ function ShoppingCartPopup(props) {
         <div className="cart-container" onClick={e => set_visible(false)}>
 
             <div className="cart-details" onClick={e => e.stopPropagation()}>
-                <h3>SHOPPING CART</h3>
-                <hr/>
                 {
-                    shopping_list.map(data => <CartItem key={data.id} data={data}/>)
+                    count > 0?
+                        <Fragment>
+                            <h3>SHOPPING CART</h3>
+                            <hr/>
+
+                            {
+                                shopping_list.map(data => <CartItem key={data.id} data={data}/>)
+                            }
+                            <hr/>
+
+                            <h2 className="price">${numberWithCommas(total)}</h2>
+
+                            <small className="taxes">Shipping, taxes, and discounts will be calculated at checkout.</small>
+                            <button className="inverted">CHECK OUT</button>
+                        </Fragment>
+                        :
+                        <Fragment>
+                            <h3>YOUR CART IS CURRENTLY EMPTY.</h3>
+                            <p>Continue browsing <Link to={"/"}>here.</Link>.</p>
+                        </Fragment>
                 }
-                <hr/>
 
-                <h2 className="price">${numberWithCommas(total)}</h2>
 
-                <small className="taxes">Shipping, taxes, and discounts will be calculated at checkout.</small>
-                <button className="inverted">CHECK OUT</button>
             </div>
         </div>
     );
