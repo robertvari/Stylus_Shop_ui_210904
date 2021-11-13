584e54d990868666e4864d4c6a3ec67f5db19386
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-13 16:03:14 +0100

    update

--- src/components/Pages/CheckoutPage.js
+++ src/components/Pages/CheckoutPage.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
 
 function CheckoutPage(props) {
+    const [logged_in, set_logged_in] = useState(false)
+
     return (
         <div className="content-container">
             <div className="content-offset"/>
-            Checkout Page
+
+            <div className="checkout-container">
+                <div className="costumer-details-container">
+                    <h3>Contact information</h3>
+                    {
+                        !logged_in&& <small>Already have an account? <Link to={"/users/login"}>Log in</Link></small>
+                    }
+                </div>
+
+                <div className="shopping-cart-container">
+                    Shopping cart...
+                </div>
+
+            </div>
         </div>
     );
 }
