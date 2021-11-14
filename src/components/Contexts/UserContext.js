a5efd1df06df6e728af6cc99a74524227c9e5d8c
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-14 18:03:28 +0100

    update

--- src/components/Contexts/UserContext.js
+++ src/components/Contexts/UserContext.js
@@ -1,14 +1,28 @@
 import React, {createContext, useEffect, useState} from "react";
 import axios from "axios";
+import {useCookies} from "react-cookie";
 
 export const UserContext = createContext(true)
 
 export const UserProvider = (props) => {
     const [logged_in, set_set_logged_in] = useState(false)
     const [token, set_token] = useState(null)
+    const [cookies, setCookie, removeCookie] = useCookies()
 
     const API_URL = process.env.REACT_APP_API_URL
 
+    const _store_token = (value) => {
+        set_token(value)
+        setCookie("token", value, {path:"/", sameSite:"strict", maxAge:86400})
+        console.log("_store_token", value)
+    }
+    
+    const _remove_token = () => {
+        removeCookie("token", {path: "/"})
+        set_token(null)
+        set_set_logged_in(false)
+    }
+    
     const log_in_user = async (email, password) => {
         await axios({
             method: "post",
@@ -18,13 +32,24 @@ export const UserProvider = (props) => {
                 password: password
             }
         })
-            .then(res => console.log(res.data.key))
+            .then(res => _store_token(res.data.key))
             .catch(error => console.log(error.response.status))
+    }
+
+    const logout_user = async () => {
+        await axios({
+            method: "post",
+            url: `${API_URL}/api/auth/logout/`,
+            headers:{
+                authorization: `token ${token}`
+            }
+        }).then(res => _remove_token())
     }
 
     return (
         <UserContext.Provider value={{
-            log_in_user: log_in_user
+            log_in_user: log_in_user,
+            logout_user: logout_user
         }}>
             {props.children}
         </UserContext.Provider>
