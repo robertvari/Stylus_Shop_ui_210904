1b0b37de4b332b8e51409ccd88363792125c6e97
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-27 17:00:26 +0100

    update

--- src/components/Users/Profile.js
+++ src/components/Users/Profile.js
@@ -28,15 +28,15 @@ function Profile(props) {
                 <hr/>
 
                 <div className="name-container">
-                    <input type="text" placeholder="First name"/>
-                    <input type="text" placeholder="Last name"/>
+                    <input type="text" placeholder="First name" value={first_name} onChange={e => set_first_name(e.target.value)}/>
+                    <input type="text" placeholder="Last name" value={last_name} onChange={e => set_last_name(e.target.value)}/>
                 </div>
 
-                <input type="text" placeholder="Company"/>
-                <input type="text" placeholder="Address"/>
-                <input type="text" placeholder="City"/>
-                <input type="text" placeholder="Post code"/>
-                <input type="text" placeholder="Phone"/>
+                <input type="text" placeholder="Company" value={company} onChange={e => set_company(e.target.value)}/>
+                <input type="text" placeholder="Address" value={address} onChange={e => set_address(e.target.value)}/>
+                <input type="text" placeholder="City" value={city} onChange={e => set_city(e.target.value)}/>
+                <input type="text" placeholder="Post code" value={post_code} onChange={e => set_post_code(e.target.value)}/>
+                <input type="text" placeholder="Phone" value={phone} onChange={e => set_phone(e.target.value)}/>
 
                 <hr/>
 
