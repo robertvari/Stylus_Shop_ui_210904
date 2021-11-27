8bb3e07390f4a6b17672b6b8914f3263c807a01e
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-27 15:34:07 +0100

    update

--- src/components/Users/Profile.js
+++ src/components/Users/Profile.js
@@ -4,7 +4,26 @@ function Profile(props) {
     return (
         <div className="content-container">
             <div className="content-offset"/>
-            <h1>User Profile page....</h1>
+            
+            <form className="profile-form">
+                <h2>Profile</h2>
+                <hr/>
+
+                <div className="name-container">
+                    <input type="text" placeholder="First name"/>
+                    <input type="text" placeholder="Last name"/>
+                </div>
+
+                <input type="text" placeholder="Company"/>
+                <input type="text" placeholder="Address"/>
+                <input type="text" placeholder="City"/>
+                <input type="text" placeholder="Post code"/>
+                <input type="text" placeholder="Phone"/>
+
+                <hr/>
+
+                <button className="inverted">UPDATE</button>
+            </form>
         </div>
     );
 }
